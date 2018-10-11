f773b944dbe65634c2bf14e17753d85190a9502e
Jaakko <jaakko.antola@yahoo.fi>
2018-10-11 08:36:50 +0300

    testataan
    testin vuoksi

---
+++ Assets/nappulat/yellowShrink.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yellowShrink : MonoBehaviour
+{
+
+    public float shrinkSpeed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.localScale = Vector3.one * 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+
+        if (transform.localScale.x < .01f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
